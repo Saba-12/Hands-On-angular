df71fa6acfed4fe915908af461a32de2e97eb1e7
Saba-12 <101691122+Saba-12@users.noreply.github.com>
2022-03-28 10:47:16 +0530

    commit

--- Practise-Lab-1/PractiseLab1/src/app/angular/angular.component.ts
+++ Practise-Lab-1/PractiseLab1/src/app/angular/angular.component.ts
@@ -14,13 +14,12 @@ export class AngularComponent implements OnInit {
 
   title:string="Angular";
 
-  courses:Course= {name:"Seher",
+  courses:Course= {
+  name:"Seher",
   topicName:["JS","HTML","C"],
   duration:2,
   startDate:"23/02/2022",
   endDate:"25/04/2022"}
-
-
 
 
   constructor() { }
